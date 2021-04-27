a79026d98e5a261132e8f6bd833438a4fec51bb6
Anton Kurbet <anton.kurbet@gmail.com>
2021-04-27 09:15:00 +0300

    data schema

---
+++ src/main/java/ru/geekbrains/market2/entities/Category.java
@@ -0,0 +1,42 @@
+package ru.geekbrains.market2.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @ManyToMany(mappedBy = "category")
+    private List<Product> products;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    public Category(String title) {
+        this.title = title;
+    }
+}
