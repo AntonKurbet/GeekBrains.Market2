8c60b23408dd7ee5b38a0ffdedc762c9bcedae15
Anton Kurbet <anton.kurbet@gmail.com>
2021-07-01 22:30:11 +0300

    Front & fixes

--- eureka-server/src/main/java/ru/geekbrains/market2/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/ru/geekbrains/market2/eurekaserver/EurekaServerApplication.java
@@ -3,22 +3,11 @@ package ru.geekbrains.market2.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
-	@EnableWebSecurity
-	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().disable();
-		}
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
-
 }
--- front-server/src/main/java/ru/geekbrains/market2/frontserver/FrontServerApplication.java
+++ front-server/src/main/java/ru/geekbrains/market2/frontserver/FrontServerApplication.java
@@ -1,13 +1,13 @@
-package ru.geekbrains.market2.routing;
+package ru.geekbrains.market2.frontserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RoutingApplication {
+public class FrontServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RoutingApplication.class, args);
+        SpringApplication.run(FrontServerApplication.class, args);
     }
 
 }
